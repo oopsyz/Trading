0d377d07011a97a5d2b509f6b74608a4116f7bfd
zhong yang <yangz100@hotmail.com>
2024-01-05 10:09:43 -0500

    a

--- cartpole.py
+++ cartpole.py
@@ -3,9 +3,13 @@ import numpy as np
 import time
 from stable_baselines3 import A2C
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 # create environment
 env=gym.make('CartPole-v1',render_mode='human')
+#env = make_vec_env("CartPole-v1", n_envs=8, vec_env_cls=SubprocVecEnv)
+
 # reset the environment, 
 # returns an initial state
 
@@ -13,28 +17,28 @@ env=gym.make('CartPole-v1',render_mode='human')
 episodeNumber=2
 timeSteps=1000
 
-#model = A2C('MlpPolicy', env, verbose=1)
-#model.learn(total_timesteps=6000)
-#model.save("cartpole")
-model=A2C.load("cartpole_DQN")
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=6000)
+model.save("cartpole")
+#model=A2C.load("cartpole")
 time.sleep(2)
 print("Done with training")
+
 for episodeIndex in range(episodeNumber):
     observation,_=env.reset()
     print(episodeIndex)
     env.render()
     appendedObservations=[]
     for timeIndex in range(timeSteps):
-        print(timeIndex)
         #random_action=env.action_space.sample()
         random_action, _states=model.predict(observation)
+        print("timeindex:{}  action:{}".format(timeIndex,random_action))
         observation, reward, terminated, interrupted, info =env.step(random_action)
         appendedObservations.append(observation)
         time.sleep(0.02)
-        '''
         if (terminated):
             print("Terminated")
             time.sleep(2)
             break
-        '''
+        
 env.close()   
