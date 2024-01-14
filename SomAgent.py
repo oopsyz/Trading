164e9da01444de425e5b888558709c9645577b53
zhong yang <yangz100@hotmail.com>
2024-01-13 20:51:41 -0500

    V 0.2 First version that works ok

---
+++ SomAgent.py
@@ -0,0 +1,35 @@
+from VZsampleEnv import MobilePhoneCarrierEnv
+from stable_baselines3 import A2C, PPO
+
+# Create an environment with a discrete action space of 1
+env = MobilePhoneCarrierEnv()
+
+# Access the action space
+action_space = env.action_space
+
+# The single possible action would typically have a value of 0
+print(f"Action value:{action_space.sample()} \nObservations:{env.observation_space.sample()}")
+print("\n Visualize obs:",env.observation_space)
+
+#env.step(3)
+#train model
+model = PPO('MultiInputPolicy', env, verbose=1, learning_rate=0.0005)
+#model.learn(total_timesteps=50000)
+#model.save("activation")
+
+del model
+
+model=PPO.load("activation")
+obs,_ = env.reset()
+# obs = FlattenObservation(obs)
+print("Reset obs:",obs["address_validation_status"],obs["device_validation_status"],obs["sim_validation_status"])
+for i in range(10):
+    action, _state = model.predict(obs, deterministic=True)
+    print("Taking action:", action)
+    obs, reward, done, terminated, info = env.step(action)
+    print("episod ", i, "obs:",obs["address_validation_status"],obs["device_validation_status"],obs["sim_validation_status"], "reward:", reward, done)
+    #env.render()
+    if done:
+      obs,_ = env.reset()
+
+#env.step(env.action_space.sample())
--- VZsampleEnv.py
+++ VZsampleEnv.py
@@ -1,5 +1,5 @@
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import random
 from enum import Enum
 
@@ -10,10 +10,11 @@ class Actions(Enum):
     DeviceValidation=1
     SimValidation=2
     Activation=3
-    AskZip=4
-    AskSim=5
-    AskIMEI=6
+#    AskZip=4
+#    AskSim=5
+#    AskIMEI=6
 
+total_rewards=0
 
 # Define environment logic
 class MobilePhoneCarrierEnv(gym.Env):
@@ -47,64 +48,85 @@ class MobilePhoneCarrierEnv(gym.Env):
     _device_validation_status = spaces.Discrete(2)  # Device
     _sim_validation_status = spaces.Discrete(2)  # SIM
 
-    #group related info together
-    _device_info = spaces.Tuple((_device_type, _manufacturer, _os))
-    _sim_info = spaces.Tuple((_sim1_type, _sim1_status))  # Fill in SIM information dimensions
-    _network_info = spaces.Tuple((_network_type,_OCS_status,_HLR_status,_ROAMGW_status,_TASPI_status))  # Fill in network information dimensions
-    _verification_status = spaces.Tuple((_address_validation_status, _device_validation_status, _sim_validation_status))
-    _customer_info = spaces.Tuple((_customer_status,_payment_status,_sub_expiration_date,_today))
-
+    reward=0
 
     def __init__(self):
         super().__init__()
         self.observation_space = spaces.Dict({
-            "device_info": self._device_info,
-            "sim_info": self._sim_info,
-            "network_info": self._network_info,
-            "verification_status": self._verification_status,
-            "customer_info": self._customer_info
+            "device_type": self._device_type,
+            "manufacturer": self._manufacturer,
+            "os": self._os,
+            "sim1_type": self._sim1_type,
+            "sim1_status": self._sim1_status,
+            "network_type": self._network_type,
+            "OCS_status": self._OCS_status,
+            "HLR_status": self._HLR_status,
+            "ROAMGW_status": self._ROAMGW_status,
+            "TASPI_status": self._TASPI_status,
+            "customer_status": self._customer_status,
+            "payment_status": self._payment_status,
+            "sub_expiration_date": self._sub_expiration_date,
+            "today": self._today,
+            "address_validation_status": self._address_validation_status,
+            "device_validation_status": self._device_validation_status,
+            "sim_validation_status": self._sim_validation_status
         })
+
         self.action_space = spaces.Discrete(len(Actions))
         self.current_state = self.observation_space.sample()
-        # change validation status
-        self.current_state["verification_status"] = (0, 0, 0)
+        #set all validation status to 0
+        self.current_state["address_validation_status"] = 0
+        self.current_state["device_validation_status"] = 0
+        self.current_state["sim_validation_status"] = 0
+        self.reward=0
         #self.current_state["verification_status"] = (random.randint(0, 1), random.randint(0, 1), random.randint(0, 1))
         
 
-    def reset(self):
+    def reset(self, seed=None):
+        if seed is not None:
+             self.np_random.seed(seed)
         # Reset the environment to its initial state (replace with your logic)
-        self.current_state = self.observation_space.sample()  # Example initialization
-        # Reset verification status and return initial observation
-        self.current_state["verification_status"] = (0, 0, 0)
-        return self.current_state
+        new_state = self.observation_space.sample()  # Example initialization
+      
+        #self.current_state[key] = np.array([value], dtype=int)
+        new_state["address_validation_status"] = 0
+        new_state["device_validation_status"] = 0
+        new_state["sim_validation_status"] = 0
+        self.current_state=new_state
+        self.reward=0
+        return self.current_state, {}
 
     def step(self, action):
+        new_state = self.current_state.copy()  # Create a copy of the current state
         # Reward based on action and update verification status
-        reward = 0
         done = False
-        action=0
-        if ((action==Actions.AddrValidation.value) and (self.current_state["verification_status"][0]==0)):
-                self.current_state["verification_status"]=(1, self.current_state["verification_status"][1], self.current_state["verification_status"][2])
-                reward +1
-        elif (action==Actions.DeviceValidation.value and self.current_state["verification_status"][1]==0):
-                self.current_state["verification_status"]=(self.current_state["verification_status"][0], 1,self.current_state["verification_status"][2])
-                reward +=1
-        elif (action==Actions.SimValidation.value and self.current_state["verification_status"][2]==0):
-                self.current_state["verification_status"]=(self.current_state["verification_status"][0], self.current_state["verification_status"][1], 1)
-                reward +=1
-        elif (action==Actions.Activation.value and all(self.current_state["verification_status"])):  # Activate
-            reward += 5
-            done = True  # Complete order, end episode
+
+        if (action==Actions.AddrValidation.value and new_state["address_validation_status"] ==0):
+                new_state["address_validation_status"] = 1  # Mark address as validated
+                self.reward +=1
+        elif (action==Actions.DeviceValidation.value and new_state["device_validation_status"]==0):
+                new_state["device_validation_status"]=1
+                self.reward +=1
+        elif (action==Actions.SimValidation.value and new_state["sim_validation_status"]==0):
+                new_state["sim_validation_status"]=1
+                self.reward +=1
+        elif (action==Actions.Activation.value and
+               new_state["address_validation_status"] ==1 and 
+               new_state["device_validation_status"]==1 and 
+               new_state["sim_validation_status"]==1):  # Activate
+                self.reward += 5
+                done = True  # Complete order, end episode
         else:
+            self.reward -=1
             done = False
 
         # Reward for asking for missing information
-        if action[4][0] and not self.verification_status[0]:
-            reward += 0.5  
-
-        new_state =  self._get_obs()
+        #if action[4][0] and not self.verification_status[0]:
+        #    reward += 0.5 
+        terminated = False
         info = {}  # Optional: Additional information
-        return new_state, reward, done, info
+        self.current_state = new_state
+        return self.current_state, self.reward, done, terminated, info
     
     def _get_obs(self):
         return {}
