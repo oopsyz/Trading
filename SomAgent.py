c5ffc9d1c05d01a95ab8ad8b1945b797d8dbb832
zhong yang <yangz100@hotmail.com>
2024-01-27 17:40:51 -0500

    V 0.4 produced 50%+ explained variance after running fine tuning 4 times

--- SomAgent.py
+++ SomAgent.py
@@ -13,7 +13,7 @@ def objective(trial):
     n_epochs = trial.suggest_int("n_epochs", 100, 500)
     batch_size = trial.suggest_categorical("batch_size", [32, 64, 128])
     # ... other hyperparameters
-    model = PPO("MultiInputPolicy", env, verbose=0, learning_rate=lr, n_epochs=n_epochs, batch_size=batch_size)
+    model = A2C("MultiInputPolicy", env, verbose=0, learning_rate=lr, n_epochs=n_epochs, batch_size=batch_size)
     model.learn(total_timesteps=10000)  # Adjust total_timesteps as needed
     mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
     return -mean_reward  # Minimize negative mean reward
@@ -30,33 +30,52 @@ def _test_case1():
     new_state["payment_status"] = 1
     new_state["use_existing_mdn"] = 1
     return new_state
-    
+
+def _Random_Agent():
+  t = 0
+  observation = env.reset()
+  while True:
+    t += 1
+    env.render()
+    #print(observation)
+    action = env.action_space.sample()
+    print("Doing ", Actions.get_action_type(action))
+    observation, reward, done, terminated, info = env.step(action)
+    if done:
+      env.render()
+      print(f"***Episode finished after {t+1} timesteps***")
+      observation = env.reset()
+      break
+
+
 env.reset()
 #env.step(4)
 
+#_Random_Agent()
+
 #train model
-train=False  #set to False to just load existing without training
+train=True  #set to False to just load existing without training
 if(train):
   #Hyperparameter tuning
   '''
   study = optuna.create_study(direction="minimize")
   study.optimize(objective, n_trials=20)  # Adjust n_trials as needed
   best_params = study.best_params
-  model = PPO('MultiInputPolicy', env, verbose=1, **best_params, tensorboard_log="./tensorboard_logs/")
+  model = A2C('MultiInputPolicy', env, verbose=1, **best_params, tensorboard_log="./tensorboard_logs/")
   '''
   # static hyperparameters
-  model = PPO('MultiInputPolicy', env, verbose=1, learning_rate=0.0004, tensorboard_log="./tensorboard_logs/") 
-  model.learn(total_timesteps=40000)
+  model = PPO('MultiInputPolicy', env, verbose=1, learning_rate=0.0005, ent_coef=0.9, tensorboard_log="./tensorboard_logs/") 
+  model.learn(total_timesteps=60000)
   model.save("activation")
   print("Model Saved")
   del model
 
-retrain=False
-if(retrain):
-  params = { 'learning_rate': 0.00003, 'n_steps': 1024, 'batch_size': 128, 'n_epochs': 10 }
+finetune=True
+if(finetune):
+  params = { 'learning_rate': 0.0003, 'n_steps': 1024, 'ent_coef': 0.1, 'batch_size': 128, 'n_epochs': 5 }
   model=PPO.load("activation", env, custom_objects=params)
   print(f"Hyper Params: lr:{model.learning_rate}; batch size:{model.batch_size}")
-  model.learn(total_timesteps=20000)
+  model.learn(total_timesteps=80000)
   model.save("activation")
   print("Retrained model saved")
   del model
@@ -70,15 +89,21 @@ if(testing):
   env.set_test_data(obs)
 
 print("Reset obs:",obs["address_validation_status"],obs["device_validation_status"],obs["sim_validation_status"], obs["new_mdn_status"])
-for i in range(30):
-    action, _state = model.predict(obs, deterministic=True)
+
+steps=0
+for i in range(130):
+    action, _state = model.predict(obs, deterministic=False)
+    #action = env.action_space.sample()
     print("Doing ", Actions.get_action_type(action))
     obs, reward, done, terminated, info = env.step(action)
+    steps +=1
     #print("episod ", i, "obs:",obs["address_validation_status"],obs["device_validation_status"],obs["sim_validation_status"], obs["mdn_status"],"reward:", reward, done)
     #print("mdn related to:", obs["mdn2sim"]," use existing? ",obs["use_existing_mdn"])
     env.render(mode="human")
     if done:
+      print(f"Completed in {steps} steps")
       obs,_ = env.reset()
+      steps=0
       #break
 
 #env.step(env.action_space.sample())
--- VZsampleEnv.py
+++ VZsampleEnv.py
@@ -12,6 +12,7 @@ class Actions(Enum):
     ReserveMDN=4
     DeactivateOld=5
     MakePayment=6
+    #ActivateVAS=7
     #ServiceQualification=5
     #ServiceCatalogLookup=6
     #ServiceInventoryMgt=7
@@ -26,7 +27,8 @@ total_rewards=0
 
 # Define environment logic
 class MobilePhoneCarrierEnv(gym.Env):
-    num_envs = 1
+    num_envs = 1 #required by Stable Baselines3
+
     # Device Information
     _device_type = spaces.Discrete(3)  # Assuming 4 device types: smartphone, smartwatch, other
     _os = spaces.Discrete(3)  # iOS, Android, other
@@ -56,8 +58,11 @@ class MobilePhoneCarrierEnv(gym.Env):
     _device_validation_status = spaces.Discrete(2)  # Device
     _sim_validation_status = spaces.Discrete(2)  # SIM
 
+    _vas_status = spaces.Discrete(3) # 0: No subscription; 1: active; 2: not active
+
     reward=0
     steps=0
+    done = False
     
     def __init__(self):
         super().__init__()
@@ -83,7 +88,9 @@ class MobilePhoneCarrierEnv(gym.Env):
             "sim_validation_status": self._sim_validation_status,
             "existing_mdn_status": self._mdn_status,
             "new_mdn_status": self._mdn_status,
-            "use_existing_mdn": self._useExistingMdn 
+            #"vas_status": self._vas_status,
+            "use_existing_mdn": self._useExistingMdn
+
         })
         self.action_space = spaces.Discrete(len(Actions))
         self.current_state = self._get_obs()
@@ -99,8 +106,7 @@ class MobilePhoneCarrierEnv(gym.Env):
 
     def step(self, action):
         new_state = self.current_state.copy()  # Create a copy of the current state
-        # Reward based on action and update verification status
-        done = False
+        # Reward based on action and update verification status   
 
         if (action==Actions.AddrValidation.value and new_state["address_validation_status"] ==0):
             new_state["address_validation_status"] = 1  # Mark address as validated
@@ -137,16 +143,13 @@ class MobilePhoneCarrierEnv(gym.Env):
                 if(new_state["use_existing_mdn"]==0): 
                     if (new_state["new_mdn_status"]==3):  # Activate
                         self._activate(new_state)
-                        done = True  # Complete order, end episode
                     else:
                         self.reward -=1
                 elif (new_state["use_existing_mdn"]==1):
                     if(new_state['existing_mdn_status']==3): # using existing number
                         self._activate(new_state)
-                        done = True  # Complete order, end episode
                     elif(new_state['existing_mdn_status']==2):
                         self.reward=8
-                        done = True
                     else:
                         self.reward -=1
                 else:
@@ -171,6 +174,15 @@ class MobilePhoneCarrierEnv(gym.Env):
         else:
             self.reward -=1
 
+        if(new_state["use_existing_mdn"]==0): 
+            if(new_state['new_mdn_status']==2):
+                self.done = True
+        elif (new_state["use_existing_mdn"]==1):
+            if(new_state['existing_mdn_status']==2):
+                self.done = True
+        else:
+            self.done = False
+
         # Reward for asking for missing information
         #if action[4][0] and not self.verification_status[0]:
         #    reward += 0.5 
@@ -178,7 +190,7 @@ class MobilePhoneCarrierEnv(gym.Env):
         info = {}  # Optional: Additional information
         self.current_state = new_state
         #return self.current_state, self.reward, done, terminated, info
-        return self.current_state, self.reward, done, terminated, info
+        return self.current_state, self.reward, self.done, terminated, info
     
     def is_everything_valid(self, new_state):
         return (
@@ -194,6 +206,7 @@ class MobilePhoneCarrierEnv(gym.Env):
         new_state["sim_validation_status"] = 0
         new_state["new_mdn_status"] = 0
         self.reward=0
+        self.done=False
         #new_state=self._test_case1()
         return new_state
 
@@ -233,12 +246,9 @@ class MobilePhoneCarrierEnv(gym.Env):
             print(f"Validation address: {self.current_state['address_validation_status']}")
             print(f"Validation device: {self.current_state['device_validation_status']}")
             print(f"Validation sim: {self.current_state['sim_validation_status']}") 
-            
+            #print(f"Vas status: {self.current_state['vas_status']}")
             print(f"Reward: {self.reward}")
-            if(self.current_state["use_existing_mdn"]==1 and self.current_state['existing_mdn_status']==2):
-                print("***************Done***************")
-            elif(self.current_state["use_existing_mdn"]==0 and self.current_state['new_mdn_status']==2):
-                print("***************Done***************")
-
+            if(self.done==True):
+                print("***************Mobile Service Done***************")
             # ... (print other relevant status information)
             print("----------------------------------")
